4115a03cd28764e26dc63776aa1e812a7687df6d
HGalitska <h.galitskaya@gmail.com>
2018-08-15 10:11:56 +0300

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
+# from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+# bootstrap = Bootstrap(app)
 
 from app import routes
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
