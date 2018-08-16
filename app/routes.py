771bfc1847fdb51a1da2d428e8a7c44d0decc5de
dimabranik <dmitriybranitskiy@gmail.com>
2018-08-16 23:07:16 +0300

    Added spider for scraping airlines info, changed app/routes.py for fitting new scraper

--- app/routes.py
+++ app/routes.py
@@ -25,21 +25,7 @@ def signup():
 
 @app.route('/airlines')
 def airlinesinfo():
-    airlines = [
-        {
-            "name" : "Wizzair",
-            "since" : "2003",
-            "origin": "Hungary",
-            "info" : "To represent user posts I'm using a list, where each element is a dictionary that has author and body fields. When I get to implement users and blog posts for real I'm going to try to preserve these field names as much as possible, so that all the work I'm doing to design and test the home page template using these fake objects will continue to be valid when I introduce real users and posts."
-        },
-        {
-            "name": "Ryanair",
-            "since": "2008",
-            "origin" : "Sweden",
-            "info": "To represent user posts I'm using a list, where each element is a dictionary that has author and body fields. When I get to implement users and blog posts for real I'm going to try to preserve these field names as much as possible, so that all the work I'm doing to design and test the home page template using these fake objects will continue to be valid when I introduce real users and posts."
-        }
-    ]
-    return render_template('airlines.html', airlines=airlines)
+    return render_template('airlines.html')
 
 @app.route('/search')
 def search():
