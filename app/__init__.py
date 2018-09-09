95a99d7852aaa6fe9b0aa658a2ed794ee6312cc9
dimabranik <dmitriybranitskiy@gmail.com>
2018-09-09 21:56:01 +0300

    Fixed start_urls errors, removed using updaters, added updaters_starter

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,17 @@
 from flask import Flask
-from app.updaters.airlines_news_updater import AirlinesNewsUpdater
-from app.updaters.airlines_info_updater import AirlinesInfoUpdater
-from app.conf.config import Config
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
 from arango import ArangoClient
-from app.airlines.handler import Handler
-
-from flask_mail import Mail
-
-# from app.flights_updater import FlightsUpdater
 
 import logging.config
 import yaml
+
+from app.conf.config import Config
+from app.airlines.handler import Handler
+from app.updaters.updaters_starter import UpdaterStarter
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -48,7 +46,6 @@ cursor_list_of_airlines = airlines_data_collection.keys()
 for airline in cursor_list_of_airlines:
     list_of_airlines.append(airline)
 
-
 # logging initialization (should be after psqldb init, because it used by logger
 with open('app/conf/logging.yml', 'r') as stream:
     logging_config = yaml.load(stream)
@@ -63,70 +60,29 @@ logger = logging.getLogger('logger')
 # logger.warn('warn message')
 # logger.error('error message')
 # logger.critical('critical message')
+
+
 start_urls = []
 
 print('list of airlines: ')
 for airline in list_of_airlines:
     print(airline)
-    airline_news = airlines_data_collection.get(airline)
+    airline_data = airlines_data_collection.get(airline)
+    airline_links = airline_data['links']
     print('link:')
-    link = airline_news.get('news_link')
+    link = airline_links['news_link']
     print(link)
     print()
     start_urls.append(link)
-
-
 
 print('urls:')
 for url in start_urls:
     print(url)
 
-airline_data = airlines_data_collection.get(airline)
-airline_news_data = airline_data['news']
-# self.start_urls.append((airline_news_data['links'])['news_link'])
-
-
-
-
-airline_data = airlines_data_collection.get('uia')
-
-airline_news_data = airline_data['news']
-
-print('airline_news_data: ')
-print(airline_news_data)
-print()
-
-print('airline_news_data["latest_version"]: ')
-print(airline_news_data['latest_version'])
-print()
-
-print('airline_news_data["selectors"]: ')
-print(airline_news_data['selectors'])
-print()
-
-
-print('news: ')
-news = airline_news_data['v.' + str(airline_news_data['latest_version'])]
-print(news)
-print()
-
-print('links:')
-print(airline_data['links'])
-print()
-
 search_handler = Handler()
 
-# airlines_news_updater = AirlinesNewsUpdater("Airlines News Updater")
-# airlines_news_updater.start()
-#
-# airlines_info_updater = AirlinesInfoUpdater("Airlines Info Updater")
-# airlines_info_updater.start()
-
-from app import routes
-# flights_updater = routes.FlightsUpdater("Flights Updater")
-# flights_updater.start()
-
-
+updaters_starter = UpdaterStarter("Updater Starter")
+updaters_starter.start()
 
 if __name__ == '__main__':
     app.run(debug=True)
