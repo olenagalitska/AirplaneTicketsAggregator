05b19c21801fd449d61ad15e3fa8e3985a37a16c
dimabranik <dmitriybranitskiy@gmail.com>
2018-09-11 15:08:44 +0300

    Added method that creates db fields for stats for current and next year

--- app/dbmanager/airlines_manager.py
+++ app/dbmanager/airlines_manager.py
@@ -1,28 +1,48 @@
-from app import arangodb
+from app import arangodb, list_of_airlines
+import datetime
 
 
 class AirlinesManager:
+    def create_stats_fields(self):
+        airlines_data_collection = arangodb.collection('airlines_data')
+        curr_year = datetime.datetime.now().year
+        next_year = curr_year + 1
+        curr_year = 'year_' + str(curr_year)
+        next_year = 'year_' + str(next_year)
+
+        for airline in list_of_airlines:
+            airline_data = airlines_data_collection.get(airline)
+
+            if 'stats' not in airline_data:
+                airline_data['stats'] = {}
+                stats = airline_data['stats']
+                stats[curr_year] = {"counters": [0] * 12}
+                stats[next_year] = {"counters": [0] * 12}
+            else:
+                stats = airline_data['stats']
+                if curr_year not in stats:
+                    stats[curr_year] = {"counters": [0] * 12}
+
+                if next_year not in stats:
+                    stats[next_year] = {"counters": [0] * 12}
+
+            airline_data['stats'] = stats
+            airlines_data_collection.update(airline_data)
+
     def increase_count(self, airline, date):
         print("in increase")
         print(date)
-        airlines_data = arangodb.collection('airlines_data')
-        if airlines_data.has(airline.lower()):
-            airline_data = airlines_data.get(airline.lower())
+        airlines_data_collection = arangodb.collection('airlines_data')
+        if airlines_data_collection.has(airline.lower()):
+            airline_data = airlines_data_collection.get(airline.lower())
             date_parts = date.split('-')
 
             year = "year_" + date_parts[0]
             month = int(date_parts[1])
             stats = airline_data['stats']
-            try:
-                year_object = stats[year]
-            except Exception:
-                stats[year] = {"counters": [0] * 12}
-                airline_data['stats'] = stats
-                airlines_data.update(airline_data)
-                airline_data = airlines_data.get(airline.lower())
-                year_object = stats[year]
+            year_object = stats[year]
             months = year_object['counters']
             months[month - 1] = months[month - 1] + 1
             year_object['counters'] = months
             airline_data['stats'][year] = year_object
-            airlines_data.update(airline_data)
+            airlines_data_collection.update(airline_data)
