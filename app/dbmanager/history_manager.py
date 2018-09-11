f327374a5d03b9b77c81a9ae9035649badaf1880
dimabranik <dmitriybranitskiy@gmail.com>
2018-09-11 15:11:57 +0300

    Refactored code

--- app/dbmanager/history_manager.py
+++ app/dbmanager/history_manager.py
@@ -1,20 +1,21 @@
 from app import arangodb
 
+
 class HistoryManager:
     def insert_history(self, key, search):
-        history = arangodb.collection('history')
-        search_found = history.get(key)
+        history_collection = arangodb.collection('history')
+        search_found = history_collection.get(key)
         if search_found is None:
-            history.insert(search)
+            history_collection.insert(search)
 
     def remove_history(self, key, user_id):
-        user_activity = arangodb.collection('user_activity')
+        user_activity_collection = arangodb.collection('user_activity')
         # history_flights = arangodb.collection('history')
-        user_document = user_activity.get(str(user_id))
+        user_document = user_activity_collection.get(str(user_id))
         search_ids = user_document['searches']
         for id in search_ids:
             if id == key:
                 search_ids.remove(id)
-                break;
+                break
         user_document['searches'] = search_ids
-        user_activity.update(user_document)
+        user_activity_collection.update(user_document)
--- app/dbmanager/user_activity_manager.py
+++ app/dbmanager/user_activity_manager.py
@@ -1,25 +1,24 @@
 from app import arangodb
 from app.models import Flight
 
+
 class UserActivityManager:
     def init_user(self, user_id):
-        user_activity = arangodb.collection('user_activity')
+        user_activity_collection = arangodb.collection('user_activity')
         activity = {'_key': str(user_id), 'flights': [], 'searches': []}
-        user_activity.insert(activity)
-
+        user_activity_collection.insert(activity)
 
     def insert_flight(self, flight_id, user_id):
-        user_activity = arangodb.collection('user_activity')
-        activity = user_activity.get(str(user_id))
+        user_activity_collection = arangodb.collection('user_activity')
+        activity = user_activity_collection.get(str(user_id))
         list_of_flights = activity['flights']
         list_of_flights.append(flight_id)
         activity['flights'] = list_of_flights
-        user_activity.update(activity)
-
+        user_activity_collection.update(activity)
 
     def insert_search(self, key, user_id):
-        user_activity = arangodb.collection('user_activity')
-        user_document = user_activity.get(str(user_id))
+        user_activity_collection = arangodb.collection('user_activity')
+        user_document = user_activity_collection.get(str(user_id))
         list_of_searches = user_document['searches']
         already_in_history = False
 
@@ -31,23 +30,22 @@ class UserActivityManager:
         if not already_in_history:
             list_of_searches.append(key)
             user_document['searches'] = list_of_searches
-            user_activity.update(user_document)
-
+            user_activity_collection.update(user_document)
 
     def get_user_history(self, user_id):
-        user_activity = arangodb.collection('user_activity')
-        history_flights = arangodb.collection('history')
-        user_document = user_activity.get(str(user_id))
+        user_activity_collection = arangodb.collection('user_activity')
+        history_collection = arangodb.collection('history')
+        user_document = user_activity_collection.get(str(user_id))
         search_ids = user_document['searches']
         list_of_searches = []
         for id in search_ids:
-            search = history_flights.get(id)
+            search = history_collection.get(id)
             list_of_searches.append(search)
         return list_of_searches
 
     def get_saved_flights(self, user_id):
-        user_activity = arangodb.collection('user_activity')
-        user_document = user_activity.get(str(user_id))
+        user_activity_collection = arangodb.collection('user_activity')
+        user_document = user_activity_collection.get(str(user_id))
         flights_ids = user_document['flights']
         list_of_flights = []
         for id in flights_ids:
