fb21e2b4896d58bfde49f7c22b64ce4710ab966e
dimabranik <dmitriybranitskiy@gmail.com>
2018-09-11 13:40:51 +0300

    Made some fixes

--- app/dbmanager/airlines_manager.py
+++ app/dbmanager/airlines_manager.py
@@ -1,27 +1,28 @@
 from app import arangodb
 
+
 class AirlinesManager:
     def increase_count(self, airline, date):
         print("in increase")
         print(date)
-        airlines = arangodb.collection('airlines_data')
-        if airlines.has(airline.lower()):
-            airline_data = airlines.get(airline.lower())
+        airlines_data = arangodb.collection('airlines_data')
+        if airlines_data.has(airline.lower()):
+            airline_data = airlines_data.get(airline.lower())
             date_parts = date.split('-')
-            if len(date_parts) == 3:
-                year = "year_" + date_parts[0]
-                month = int(date_parts[1])
-                stats = airline_data['stats']
+
+            year = "year_" + date_parts[0]
+            month = int(date_parts[1])
+            stats = airline_data['stats']
+            try:
                 year_object = stats[year]
-                if year_object is None:
-                    stats[year] = {"counters" : [0] * 12}
-                    airlines.update(airline_data)
-                    year_object = stats[year]
-                months = year_object['counters']
-                months[month - 1] = months[month - 1] + 1
-                year_object['counters'] = months
-                airlines.update(airline_data)
-
-
-
-
+            except Exception:
+                stats[year] = {"counters": [0] * 12}
+                airline_data['stats'] = stats
+                airlines_data.update(airline_data)
+                airline_data = airlines_data.get(airline.lower())
+                year_object = stats[year]
+            months = year_object['counters']
+            months[month - 1] = months[month - 1] + 1
+            year_object['counters'] = months
+            airline_data['stats'][year] = year_object
+            airlines_data.update(airline_data)
