4c5e140ac73e0dbb9d48e2ff96882eeef6cb4073
HGalitska <h.galitskaya@gmail.com>
2018-09-12 18:00:46 +0300

    added small fixes

--- app/dbmanager/user_activity_manager.py
+++ app/dbmanager/user_activity_manager.py
@@ -6,13 +6,13 @@ from app.dbmanager.saved_flights_manager import SavedFlightsManager
 class UserActivityManager:
     def init_user(self, user_id):
         user_activity_collection = arangodb.collection('user_activity')
-        activity = {'_key': str(user_id), 'flights': [], 'searches': []}
+        activity = {'_key': str(user_id), 'saved_flights': [], 'searches': []}
         user_activity_collection.insert(activity)
 
     def insert_flight(self, flight_id, user_id):
         user_activity_collection = arangodb.collection('user_activity')
         activity = user_activity_collection.get(str(user_id))
-        list_of_flights = activity['flights']
+        list_of_flights = activity['saved_flights']
 
         already_in_flights = False
         for flight in list_of_flights:
@@ -21,8 +21,8 @@ class UserActivityManager:
                 break
         if not already_in_flights:
             list_of_flights.append(flight_id)
-            activity['flights'] = list_of_flights
-            user_activity.update(activity)
+            activity['savedflights'] = list_of_flights
+            user_activity_collection.update(activity)
             savedFlightsManager = SavedFlightsManager()
             savedFlightsManager.add_saved_flight(flight_id, user_id)
 
@@ -56,7 +56,7 @@ class UserActivityManager:
     def get_saved_flights(self, user_id):
         user_activity_collection = arangodb.collection('user_activity')
         user_document = user_activity_collection.get(str(user_id))
-        flights_ids = user_document['flights']
+        flights_ids = user_document['saved_flights']
         list_of_flights = []
         for id in flights_ids:
             flight = Flight.query.filter_by(id=id).first()
